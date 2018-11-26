6915603acdb24d12784febdb92086fc7fdfb3aa6
aayyeah <444.fck@gmail.com>
2018-11-26 21:55:37 +1000

    Реализовать задания 8-11

--- HW4 Bit operations/tasks.cpp
+++ HW4 Bit operations/tasks.cpp
@@ -5,16 +5,16 @@
 typedef unsigned short  int UINT;
 
 // Ввести целое число N и вывести 2^N, используя битовые операции.
-int stepen(UINT n)
+int task1(UINT n)
 {
     UINT a = 2;
     a = a << (n-1);
-    std::cout << "2^n = " << a << std::endl;
+    std::cout << "task1: " << "2^n = " << a << std::endl;
     return 0;
 }
 
 // Вывести двоичное представление целого положительного числа
-int bitvivod(UINT n)
+int task2(UINT n)
 {
     std::string otvet;
     UINT a = 1;
@@ -30,12 +30,12 @@ int bitvivod(UINT n)
         }
         n = n - (n/2 + (n&a));
     }
-    std::cout << "Dvoichnoe predstavlenie = " << otvet << std::endl;
+    std::cout << "task2: " << "Dvoichnoe predstavlenie = " << otvet << std::endl;
     return 0;
 }
 
 // Найти количество едениц в двоичном представлении числа
-int onecounter(UINT n)
+int task3(UINT n)
 {
     UINT a = 1;
     UINT c = 0;
@@ -48,12 +48,12 @@ int onecounter(UINT n)
 
         n = n - (n/2 + (n&a));
     }
-    std::cout << "kol-vo edenic = " << c << std::endl;
+    std::cout << "task3: " << "kol-vo edenic = " << c << std::endl;
     return 0;
 }
 
 // Найти номер старшего бита
-int upnumber(UINT n)
+int task4(UINT n)
 {
     UINT a = 1;
     UINT c = 0;
@@ -62,26 +62,26 @@ int upnumber(UINT n)
         c++;
         n = n - (n/2 + (n&a));
     }
-    std::cout << "Nomer starshego bita = " << c-1 << std::endl;
+    std::cout << "task4: " <<  "Nomer starshego bita = " << c-1 << std::endl;
     return 0;
 }
 
 // число n, где i-й бит = 1
 // оставил bitset для проверки.
-int replacei(UINT n, UINT i)
+int task5(UINT n, UINT i)
 {
     i = 1 << i;
     // std::cout <<  std::bitset<8>(i) << std::endl;
     // std::cout <<  std::bitset<8>(n) << std::endl;
     n = n|i;
     // std::cout <<  std::bitset<8>(n) << std::endl;
-    std::cout << "n = " << n << std::endl;
+    std::cout << "task5: " << "result = " << n << std::endl;
     return 0;
 }
 
 // число n, где i-й бит = 0
 // оставил bitset для проверки.
-int outplacei(UINT n, UINT i)
+int task6(UINT n, UINT i)
 {
     i = 1 << i;
     i = ~i;
@@ -89,12 +89,12 @@ int outplacei(UINT n, UINT i)
     // std::cout <<  std::bitset<8>(n) << std::endl;
     n = n&i;
     // std::cout <<  std::bitset<8>(n) << std::endl;
-    std::cout << "n = " << n << std::endl;
+    std::cout << "task6: " << "result = " << n << std::endl;
     return 0;
 }
 
-// число n, где i-1 бит отличается
-int diffi(UINT n, UINT i)
+// число n, где i-й бит отличается
+int task7(UINT n, UINT i)
 {
     UINT a = n;
     UINT s = i;
@@ -110,7 +110,72 @@ int diffi(UINT n, UINT i)
         n = n + (2 << (s -1));
     }
 
-    std::cout << "n = " << n <<  std::endl;
+    std::cout << "task7: " << "result = " << n <<  std::endl;
     return 0;
+}
+
+// циклический сдвиг влево на 1 разряд
+int task8(unsigned char n)
+{
+    if ((128 & n ) == (128))
+    {
+        n = n << 1;
+        n = n + 1;
+    }
+    else
+    {
+        n = n << 1;
+    }
+    std::cout << "task8: " << "result = " << (int)(n) << std::endl;
+    return 0;
+}
+
+// сколько раз встречается 11 в двоичном представлении целого положительного числа
+int task9 (UINT n)
+{
+    int counter = 0;
+    while (n >= 1)
+    {
+       if ((n & 3) == 3)
+       {
+           counter++;
+       }
+       n = n >> 1;
+    }
+    std::cout << "task9: result = " << counter << std::endl;
+    return 0;
+}
+
+// в числе n сменить местами бит b1 и b2
+int task10 (UINT n, UINT b1, UINT b2)
+{
+    if (((n & (1 << b1)) >> b1) > ((n & (1 << b2)) >> b2))
+    {
+        n = n - (1 << b1) + (1 << b2);
+    }
+    else
+    {
+        if (((n & (1 << b1)) >> b1) < ((n & (1 << b2)) >> b2))
+        {
+            n = n + (1 << b1) - (1 << b2);
+        }
+    }
+    std::cout << "task10: result = " << n << std::endl;
+    return 0;
+}
+
+// Вырвать бит
+int task11 (UINT n, UINT i)
+{
+    UINT perem = 1;
+    for(UINT l = 0; l < i; ++l)
+    {
+        perem = perem + ( 2 << l-1);
+    }
+    n = n - perem;
+    n = n >> 1;
+    n = n + perem;
+    std::cout << "task11: result = " << n << std::endl;
+    return  0;
 }
 
--- HW4 Bit operations/tasks.h
+++ HW4 Bit operations/tasks.h
@@ -3,18 +3,26 @@
 
 typedef unsigned short  int UINT;
 
-int stepen(UINT n);
+int task1(UINT n);
 
-int bitvivod(UINT n);
+int task2(UINT n);
 
-int onecounter(UINT n);
+int task3(UINT n);
 
-int upnumber(UINT n);
+int task4(UINT n);
 
-int replacei(UINT n,UINT i);
+int task5(UINT n,UINT i);
 
-int outplacei(UINT n, UINT i);
+int task6(UINT n, UINT i);
 
-int diffi(UINT n, UINT i);
+int task7(UINT n, UINT i);
+
+int task8(unsigned char n);
+
+int task9(UINT n);
+
+int task10 (UINT n, UINT b1, UINT b2);
+
+int task11 (UINT n, UINT i);
 
 #endif //POBITOVIE_OPERACII_TASKS_H
